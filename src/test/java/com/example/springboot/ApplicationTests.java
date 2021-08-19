30533393fd59dff75f38b9419202054f2bbec19c
SergioQuezadaNubox <sergio.quezada@nubox.com>
2021-08-19 15:41:06 -0400

    First Commit

---
+++ src/test/java/com/example/springboot/ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springboot;
+import com.example.springboot.HelloController;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+	public void contextLoads() {
+	}
+
+}
